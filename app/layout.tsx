7a8eada8893e13685a5c9882d9fffa271c362a67
vikaswakde <vikaswakdepc@gmail.com>
2025-06-29 01:40:21 +0530

    'disabled default theme'

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
